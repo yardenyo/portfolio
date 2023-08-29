0a9c10ce2aabd70fb8815378417ed494a77e2f37
yardenyo <gmloler@gmail.com>
2023-08-29 21:37:43 +0300

    responsive home div

--- src/components/HomeMainDiv.vue
+++ src/components/HomeMainDiv.vue
@@ -1,11 +1,20 @@
 <template>
   <div id="large-header" class="large-header">
     <canvas id="demo-canvas"> </canvas>
+    <img
+      src="src/assets/img/portrait.png"
+      alt="Portrait Image"
+      class="portrait-image"
+    />
     <div class="title-wrapper">
-      <h1 class="main-title">Yarden <span class="thin">Yosef.</span></h1>
+      <div class="main-title">
+        <h1 class="title">Yarden <span class="thin">Yosef.</span></h1>
+      </div>
       <div class="description-container">
-        <div class="description-title"></div>
-        <!-- This element will be animated -->
+        <div class="static-text">I Am</div>
+        <div class="description-title">
+          <span class="typed-text"></span>
+        </div>
       </div>
     </div>
   </div>
@@ -21,7 +30,7 @@ onMounted(() => {
     "Creating amazing web experiences.",
   ];
 
-  const descriptionTitle = document.querySelector(".description-title");
+  const typedText = document.querySelector(".typed-text");
 
   let currentIndex = 0;
 
@@ -221,12 +230,14 @@ onMounted(() => {
 
   function animateDescription() {
     const currentDescription = descriptions[currentIndex];
-    descriptionTitle.textContent = "";
+    typedText.textContent = "";
 
     typeText(currentDescription, () => {
       setTimeout(() => {
-        currentIndex = (currentIndex + 1) % descriptions.length;
-        animateDescription();
+        eraseText(() => {
+          currentIndex = (currentIndex + 1) % descriptions.length;
+          animateDescription();
+        });
       }, 1000);
     });
   }
@@ -237,7 +248,7 @@ onMounted(() => {
 
     function type() {
       if (index < text.length) {
-        descriptionTitle.textContent += text.charAt(index);
+        typedText.textContent += text.charAt(index);
         index++;
         setTimeout(type, typingSpeed);
       } else {
@@ -246,6 +257,24 @@ onMounted(() => {
     }
 
     type();
+  }
+
+  function eraseText(callback) {
+    let text = typedText.textContent;
+    let index = text.length;
+    const erasingSpeed = 30;
+
+    function erase() {
+      if (index > 0) {
+        typedText.textContent = text.substring(0, index - 1);
+        index--;
+        setTimeout(erase, erasingSpeed);
+      } else {
+        callback();
+      }
+    }
+
+    erase();
   }
 
   animateDescription();
@@ -262,6 +291,14 @@ onMounted(() => {
   z-index: 1;
 }
 
+.portrait-image {
+  position: absolute;
+  bottom: 0;
+  left: 10%;
+  height: 100%;
+  opacity: 0.4;
+}
+
 .title-wrapper {
   position: absolute;
   top: 50%;
@@ -271,7 +308,7 @@ onMounted(() => {
   .main-title {
     position: relative;
     padding: 0 1rem;
-    font-size: 4rem;
+    font-size: 2rem;
     font-weight: 500;
     color: $white;
     text-align: center;
@@ -281,16 +318,74 @@ onMounted(() => {
     }
   }
 
-  .description-title {
-    position: relative;
-    padding: 0 1rem;
+  .description-container {
+    margin-top: 1rem;
     font-size: 2rem;
     font-weight: 500;
     color: $white;
-    text-align: center;
-    z-index: 100;
-    .thin {
-      font-weight: 200;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    .static-text {
+      margin-right: 0.5rem;
+    }
+
+    .description-title {
+      display: inline-block;
+      position: relative;
+
+      .typed-text {
+        color: $primary;
+        font-weight: 500;
+      }
+
+      .typed-text::after {
+        content: "|";
+        display: inline-block;
+        animation: blinkCursor 0.7s infinite alternate;
+        color: $white;
+        margin-left: 5px;
+      }
+    }
+  }
+
+  @keyframes blinkCursor {
+    to {
+      opacity: 0;
+    }
+  }
+}
+
+@media screen and (max-width: $desktop-width) {
+  .portrait-image {
+    margin: 0 auto;
+    left: 0;
+    width: 100%;
+    height: auto;
+    max-width: 400px;
+  }
+
+  .title-wrapper {
+    width: 100%;
+    padding: 0 1rem;
+
+    .main-title {
+      font-size: 1.5rem;
+    }
+  }
+}
+
+@media screen and (max-width: $tablet-width) {
+  .title-wrapper {
+    .main-title {
+      font-size: 1.2rem;
+    }
+
+    .description-container {
+      display: flex;
+      flex-direction: column;
+      font-size: 1.2rem;
     }
   }
 }
