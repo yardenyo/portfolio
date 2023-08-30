b298bbbb969025fda73486dc0aef71dcf25eb73b
yardenyo <gmloler@gmail.com>
2023-08-30 14:01:33 +0300

    added whatsapp support

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,6 +9,16 @@
     <div class="skills">
       <Skills />
     </div>
+    <div class="fixed-whatsapp-button">
+      <a
+        :href="`https://wa.me/${phoneNumber}?text=${text}`"
+        class="whatsapp-button"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i class="fab fa-whatsapp"></i>
+      </a>
+    </div>
   </div>
 </template>
 
@@ -17,6 +27,11 @@ import { ref, reactive, computed, onMounted } from "vue";
 import HomeMainDiv from "@/components/HomeMainDiv.vue";
 import Navbar from "@/components/Navbar.vue";
 import Skills from "@/components/Skills.vue";
+
+const phoneNumber = ref("972527899937");
+const text = ref(
+  "Hi%20Yarden%2C%20I%20saw%20your%20portfolio%20and%20I%20want%20to%20hire%20you!"
+);
 </script>
 
 <style scoped lang="scss">
@@ -41,6 +56,44 @@ import Skills from "@/components/Skills.vue";
   .skills {
     width: 100%;
     height: 100%;
+  }
+
+  .fixed-whatsapp-button {
+    position: fixed;
+    bottom: 1rem;
+    right: 1rem;
+    z-index: 1000;
+
+    .whatsapp-button {
+      width: 4rem;
+      height: 4rem;
+      border-radius: 50%;
+      background-color: $whatsapp;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      color: $text-color-accent;
+      font-size: 2rem;
+      box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+      transition: transform 0.3s ease;
+      text-decoration: none;
+
+      &:hover {
+        transform: scale(1.1);
+      }
+    }
+  }
+}
+
+@media (max-width: $mobile-width) {
+  .home {
+    .fixed-whatsapp-button {
+      .whatsapp-button {
+        width: 3rem;
+        height: 3rem;
+        font-size: 1.5rem;
+      }
+    }
   }
 }
 </style>
